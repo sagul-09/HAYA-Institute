37dcb96a5849e2c329c1a5b6dfbea29672623497
sagul-09 <nihal.evil.royal@gmail.com>
2024-09-20 19:45:43 +0530

    added slider image

--- src/components/BackgroundSlider/BackgroundSlider.jsx
+++ src/components/BackgroundSlider/BackgroundSlider.jsx
@@ -3,13 +3,15 @@ import BackgroundSlideshow from 'react-background-slideshow';
 import image1 from '../../assets/BackgroundImages/1.jpg';
 import image2 from '../../assets/BackgroundImages/2.jpg';
 import image3 from '../../assets/BackgroundImages/3.jpg';
+import image4 from '../../assets/BackgroundImages/4.jpg';
+import image5 from '../../assets/BackgroundImages/5.jpg';
 import './Slider.css'
 
 export default class BackgroundSlider extends Component {
   render() {
     return (
       <div className="background-slider">
-        <BackgroundSlideshow images={[image1, image2, image3]} animationDelay={1000} />
+        <BackgroundSlideshow images={[image1, image2, image3, image4, image5]} animationDelay={1000} />
       </div>
     );
   }
